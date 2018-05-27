29ae36987c42ed903309ae258abd9962015dc4e8
Joey Mckenzie <joey32793@gmail.com>
2018-05-27 16:53:36 -0700

    Fixing server routing v2

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 3000);
 
-app.route('*').get(function(req, res) {
+app.get('*').get(function(req, res) {
   res.sendFile('./src/index.html');
 });
+
